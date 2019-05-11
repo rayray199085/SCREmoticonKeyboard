c9100f931ea172e2c635eb46abb669a7032d1a67
Rui Cao <rayray199085@gmail.com>
2019-05-11 15:10:33 +1000

    init emoticon keyboard view

---
+++ Emoticon/SCEmoticonKeyboard.swift
@@ -0,0 +1,21 @@
+//
+//  SCEmoticonKeyboard.swift
+//  SCREmoticonKeyboard
+//
+//  Created by Stephen Cao on 11/5/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+import UIKit
+
+public class SCEmoticonKeyboard: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
